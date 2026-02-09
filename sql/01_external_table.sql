0af520659d2833bb63da3874994e499d6d1c4641
Afra332270 <afrarahman11@gmail.com>
2026-02-09 12:33:16 +0600

    Update 01_external_table.sql

--- sql/01_external_table.sql
+++ sql/01_external_table.sql
@@ -17,20 +17,39 @@ OPTIONS (
   autodetect = TRUE
 );
 
--- Reading, Inspecting and Validating the dataset (externally)
+-- The next queries show how we read, inspect and validate the dataset (externally)
 
+
+-- Previewing the dataset
 SELECT * FROM `glowing-bird-474605-c3.raw.superstore_external` LIMIT 10;
 
+
+-- Checking for the total number of rows
 SELECT COUNT(*) FROM `glowing-bird-474605-c3.raw.superstore_external`;
 
+
+
+-- Checking for all the table names in the dataset and their type. We are using metadata fields (table_name, table_type) provided by BigQuery’s INFORMATION_SCHEMA
 SELECT
   table_name,
   table_type
 FROM `glowing-bird-474605-c3.raw.INFORMATION_SCHEMA.TABLES`;
 
 
--- Checking column names and order
 
+-- Checking for all column names and their data types. We are using metadata fields (column_name, data_type) provided by BigQuery’s INFORMATION_SCHEMA
 SELECT column_name, data_type
-FROM `project.raw.INFORMATION_SCHEMA.COLUMNS`
+FROM `glowing-bird-474605-c3.raw.INFORMATION_SCHEMA.COLUMNS`
 WHERE table_name = 'superstore_external';
+
+
+-- Checking for the total number of rows and to see if there are any missing order_id values (null values).
+SELECT
+  COUNT(*) AS total_rows,
+  COUNT(order_id) AS order_id_present
+FROM `glowing-bird-474605-c3.raw.superstore_external`;
+
+
+SELECT COUNT(*)
+FROM `glowing-bird-474605-c3.raw.superstore_external`
+WHERE SAFE_CAST(sales AS FLOAT64) IS NULL;
