2cea437dcd61452237f88cd665199af492532c82
Afra332270 <afrarahman11@gmail.com>
2026-02-10 12:43:07 +0600

    Update 02_native_table.sql

--- sql/02_native_table.sql
+++ sql/02_native_table.sql
@@ -12,6 +12,20 @@ SELECT COUNT(*) AS total_rows
 FROM `glowing-bird-474605-c3.raw.superstore_native`;
 
 
+SELECT order_id,customer_id,product_id, COUNT(*) AS cnt
+FROM `glowing-bird-474605-c3.raw.superstore_native`
+GROUP BY
+  order_id,
+  product_id,
+  customer_id
+HAVING COUNT(*) > 1;
+
+
+SELECT *
+FROM `glowing-bird-474605-c3.raw.superstore_native`
+WHERE order_id = 'CA-2017-129714';
+
+
 
 -- This query checks to see if there are any missing values in major columns by giving the count of non-missing values of each column
 SELECT
@@ -20,3 +34,32 @@ SELECT
   COUNT(order_date) AS order_date_non_null,
   COUNT(sales) AS sales_non_null
 FROM `glowing-bird-474605-c3.raw.superstore_native`;
+
+
+
+
+SELECT COUNT(*)
+FROM `glowing-bird-474605-c3.raw.superstore_native`
+WHERE sales < 0;
+
+
+
+SELECT
+  MIN(sales) AS min,
+  MAX(sales) AS max,
+  AVG(sales) AS avg
+FROM `glowing-bird-474605-c3.raw.superstore_native`;
+
+
+
+SELECT
+  *
+FROM `glowing-bird-474605-c3.raw.superstore_native` WHERE sales>10000;
+
+
+SELECT
+  *
+FROM `glowing-bird-474605-c3.raw.superstore_native` WHERE customer_name IS NULL;
+
+
+
